50ddabdea0b637f68c3e654e39c3ff8043ea9894
ftdks <108036062+ftdks@users.noreply.github.com>
2024-07-28 13:34:52 +0800

    Update tnest.js

--- data/tnest.js
+++ data/tnest.js
@@ -1,21 +1,20 @@
 const { stripIndents } = require("common-tags");
-const emoji = require("../emoji.js");
 const world = "TNEST"
-const header = (category) => stripIndents`SELL **${category}** GO ${emoji.arrow} **${world}**`
-const footer = (category) => stripIndents`I BUY ALL ITEM FOR CHEAP
-SELL **${category}** GO ${emoji.arrow} **${world}**`
+const header = (category) => stripIndents`SELL **${category}** GO <:Arrow:850540193626193941> **${world}**`
+const footer = (category) => stripIndents`SELL **${category}** GO <:Arrow:850540193626193941> **${world}**`
 
 function sell(item, category, join = "\n") {
     let items = item.split("\n").sort()
     let message = stripIndents`${header(category)}
+    
     ${items.join(join)}
     
     ${footer(category)}`
     return message
 }
 
-function buy(item, category, join = "\n") {
-  let items = item.spli("\n").sort()
+function buy(item, category = "CHEAP", join = "\n") {
+  let items = item.split("\n").sort()
   let message = stripIndents`**BUY LIST DM ME**
   ${items.map(i => 'Buy ' + i).join(join)}
   
@@ -31,6 +30,26 @@ const data = [
     Rayman Dream Forest Platform 5/1WL
     Immortals Fenyx Rising - Athena Statue 5WL`, "UBIWEEK")
   },
+  
+  {
+    ch: ["900836866373333052"],
+    msg: sell(stripIndents`Phoenix Mantle 82
+    Ultraviolet Aura 135`)
+  }
+  /*
+  {
+    ch: ["900836866373333052", "782718904056807494"], // rare items, iots
+    msg: sell(stripIndents`Magplant 5000
+    Guardian Pineapple
+    Gaia's Beacon
+    Unstable Tesseract`, "RARE")
+  },
+  
+  {
+    ch: ["806456760592105483"],
+    msg: sell(stripIndents`Super Firework
+    Super Summer Surprise`, "SUMMER")
+  },*/
   
   
 ]
