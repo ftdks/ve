1b1609aacf52294eb84ab4577e7e7029266c3f1d
ftdks <108036062+ftdks@users.noreply.github.com>
2024-07-28 13:34:34 +0800

    Update pom.js

--- data/pom.js
+++ data/pom.js
@@ -1,5 +1,4 @@
 const { stripIndents } = require("common-tags");
-const emoji = require("../emoji.js");
 
 const data = {
   gala: {
@@ -40,10 +39,16 @@ const data = {
 <:SurgicalTransfusion:892136795712659516> Transfusion 17/WL 
 <:SurgicalUltrasound:892136795570065489> Ultrasound 17/WL 
 <:SurgicalStitches:892136728607985734> Stitches 1/1 (200/115)
-SURG E 4/1 (200/45)
 
 (💯 Real Price💯 )
 CHEAP SURGICAL TOOLS AT <:Verified:1000267030550827128> **POM** <:Verified:1000267030550827128>`
+  },
+  
+  surgItem: {
+    ch: ["733050809314705458"],
+    msg: stripIndents`CHEAP SURGICAL ITEMS AT <:Verified:1000267030550827128>**POM**<:Verified:1000267030550827128>
+SURG E 4/1 (200/45)
+CHEAP SURGICAL ITEMS AT <:Verified:1000267030550827128>**POM**<:Verified:1000267030550827128>`
   },
   
   crime: {
@@ -64,17 +69,88 @@ CRIME CARD SHOP AT <:Arrow:850540193626193941> **POM**`
     ch: ["774455258407370752"],
     msg: stripIndents`Sell All Cheap Paint go **POM**
 =========================================
-🟡Paint Bucket - Yellow 20/1 ${emoji.wl}
-🔵Paint Bucket - Blue 20/1 ${emoji.wl}
-⚫Paint Bucket - Charcoal 10/1 ${emoji.wl}
-🔴Paint Bucket - Red 10/1 ${emoji.wl}
-⚪Paint Bucket - Varnish 20/1 ${emoji.wl}
-🔵Paint Bucket - Aqua 20/1 ${emoji.wl}
-🟣Paint Bucket - Purple 10/1 ${emoji.wl}
-🟢Paint Bucket - Green 20/1 ${emoji.wl}
-🖼️Portrait 12 ${emoji.wl}
+🟡Paint Bucket - Yellow 20/1 <:WL:880251447470596157>
+🔵Paint Bucket - Blue 20/1 <:WL:880251447470596157>
+⚫Paint Bucket - Charcoal 10/1 <:WL:880251447470596157>
+🔴Paint Bucket - Red 10/1 <:WL:880251447470596157>
+⚪Paint Bucket - Varnish 20/1 <:WL:880251447470596157>
+🔵Paint Bucket - Aqua 20/1 <:WL:880251447470596157>
+🟣Paint Bucket - Purple 10/1 <:WL:880251447470596157>
+🟢Paint Bucket - Green 20/1 <:WL:880251447470596157>
+🖼️Portrait 12 <:WL:880251447470596157>
 =========================================
 Sell All Cheap Paint go **POM**`
+  },
+  
+  lock: {
+    ch: ["878779329864167504"],
+    msg: stripIndents`Sell Locks Go **POM**
+
+SELL Small Lock/SL 25/1
+SELL Big Lock/BL 10/1
+SELL Huge Lock/HL 3/1
+SELL Builder Lock 1/33
+SELL Signal Jammer 1/2
+SELL Punch Jammer 1/10
+SELL Zombie Jammer 1/10
+SELL Lock Mover 1/20
+
+Sell Locks Go **POM**`
+  },
+  
+  jammer: {
+    ch: ["806523338797219860"],
+    msg: stripIndents`Sell All Jammer Go ➡️**POM**⬅️
+🔸Signal Jammer - 1 :WL: or 1,3 <:WL:880251447470596157> 
+🔸Punch Jammer - 8 <:WL:880251447470596157>
+🔸Zombie Jammer - 8 <:WL:880251447470596157>
+🔸Ghost Charm - 30 <:WL:880251447470596157>
+🔸Fire House - 5 <:WL:880251447470596157>
+🔸Starship Security Camera - 4 <:WL:880251447470596157>
+🔸Security Camera - 5 <:WL:880251447470596157>
+🔸Pigeon - 4 <:WL:880251447470596157>
+🔸Vip Door - 24 <:WL:880251447470596157>
+🔸Antigravity - 160 <:WL:880251447470596157>
+🔸Door Mover - 3 <:WL:880251447470596157>
+🔸Mini Mod - 70 <:WL:880251447470596157>
+Sell All Jammer Go ➡️**POM**⬅️`
+  },
+  
+  hand: {
+    ch: ["889195913275408455"],
+    msg: stripIndents`> Try your Luck :four_leaf_clover: (All Sabers For Sale)
+> 
+> ===> Go **POM** <===
+> • Sell Red growsaber.
+> • Sell Green growsaber.
+> • Sell Blue growsaber.
+> • Sell Black growsaber.
+> • Sell double growsaber.
+> • Sell flame growsaber.
+> ===> Go **POM** <===`
+  },
+  
+  block: {
+    ch: ["1104057271215984741"],
+    msg: stripIndents`BLOCK SHOP AT >>>> **POM** <<<<
+SELL CRYSTAL BLOCK 18 <:WL:880251447470596157>  EACH
+SELL RUBY BLOCK 20 <:WL:880251447470596157>  EACH
+SELL EMERALD BLOCK 20 <:WL:880251447470596157>  EACH
+SELL SHIFTY 15 <:WL:880251447470596157>  EACH
+SELL SAPPHIRE BLOCK 6 <:WL:880251447470596157>  EACH 
+SELL CHANDELIER BLOCK 30/<:WL:880251447470596157> 
+SELL ROULETTE WHEEL 10/<:WL:880251447470596157>
+SELL DISPLAY BLOCK 3/<:WL:880251447470596157>
+SELL DISPLAY BOX 6/<:WL:880251447470596157>
+SELL FORCEFIELD 3/<:WL:880251447470596157>
+SELL DRAGON GATE 10/<:WL:880251447470596157>
+SELL CUTAWAY BLOCK 50/<:WL:880251447470596157>
+SELL GLOWY BLOCK 10/<:WL:880251447470596157>
+SELL CLOUDS BLOCK 10/<:WL:880251447470596157>
+SELL TOASTED VILLAGE WALL 3/<:WL:880251447470596157>
+SELL ALL COLOR BLOCKS
+SELL NUMBER BLOCK
+GO >>>>>>> **POM** <<<<<<<`
   }
 }
 
